cbd129c34e42295c3c24623ec409da89a0019e6e
Anthony Veaudry <anthony@veaudry.pro>
2017-10-13 13:09:35 +0300

    change class name of slides
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- source/js/slideshow.js
+++ source/js/slideshow.js
@@ -7,7 +7,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("slide");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
