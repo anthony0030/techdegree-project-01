9625be15e4e1719f5c3993596af153648855c2e8
Anthony Veaudry <anthony@veaudry.pro>
2017-10-13 11:47:12 +0300

    adds slide show
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

---
+++ source/js/slideshow.js
@@ -0,0 +1,18 @@
+var slideIndex = 1;
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  document.getElementById("country").innerHTML = x[slideIndex-1].name
+  x[slideIndex-1].style.display = "block";  
+}
