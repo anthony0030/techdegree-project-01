342d6c79cb77d87b57ca03a216cc78e0bef1aa04
Anthony Veaudry <anthony@veaudry.pro>
2017-11-29 11:29:39 +0200

    change animation setting
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- docs/js/script.js
+++ docs/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   $(".animsition").animsition({
     inClass: 'fade-in',
     outClass: 'fade-out',
-    inDuration: 1500,
+    inDuration: 1000,
     outDuration: 800,
     linkElement: '.animsition-link',
     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
@@ -15,14 +15,14 @@ $(document).ready(function() {
     loadingClass: 'animsition-loading',
     loadingInner: '', // e.g '<img src="loading.svg" />'
     timeout: true,
-    timeoutCountdown: 3000,
+    timeoutCountdown: 2000,
     onLoadEvent: true,
     browser: [ 'animation-duration', '-webkit-animation-duration'],
     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
     overlay : false,
     overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
+    overlayParentElement : 'main',
     transition: function(url){ window.location.href = url; }
   });
 });
