60d7d8af496cb87edcf2e1f8a95860b378be8d09
Anthony Veaudry <anthony@veaudry.pro>
2021-06-14 11:25:16 +0300

    fixes some code
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,28 +1,28 @@
-function active_menu_item(n){
-  document.getElementById(n).classList.add("active")
+function activeMenuItem(n){
+  document.getElementById(n).classList.add("active");
 }
 
 $(document).ready(function() {
   $(".animsition").animsition({
-    inClass: 'fade-in',
-    outClass: 'fade-out',
+    inClass: "fade-in",
+    outClass: "fade-out",
     inDuration: 1000,
     outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    linkElement: ".animsition-link",
+    // e.g. linkElement: "a:not([target="_blank"]):not([href^="#"])"
     loading: true,
-    loadingParentElement: 'main', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
+    loadingParentElement: "main", //animsition wrapper element
+    loadingClass: "animsition-loading",
+    loadingInner: "", // e.g "<img src="loading.svg" />"
     timeout: true,
     timeoutCountdown: 2000,
     onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    browser: [ "animation-duration", "-webkit-animation-duration"],
     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
     overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'main',
-    transition: function(url){ window.location.href = url; }
+    overlayClass : "animsition-overlay-slide",
+    overlayParentElement : "main",
+    transition: (url) => { window.location.href = url; }
   });
 });
--- docs/js/slideshow.js
+++ docs/js/slideshow.js
@@ -8,11 +8,11 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slide");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = x.length;}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";
   }
-  document.getElementById("country").innerHTML = x[slideIndex-1].title
-  x[slideIndex-1].style.display = "block";  
+  document.getElementById("country").innerHTML = x[slideIndex-1].title;
+  x[slideIndex-1].style.display = "block";
 }
